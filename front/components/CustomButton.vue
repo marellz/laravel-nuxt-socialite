0b8ae4af72a0714716a26ad5c73dc5f7ec7a65b9
Dave Njoroge <davenjoroge10@gmail.com>
2022-09-22 13:41:34 +0300

    components

---
+++ front/components/CustomButton.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="py-2 px-5 text-white inline-block font-medium rounded bg-blue-500">
+        <slot/>
+    </button>
+</template>
---
+++ front/components/form/input.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="mb-4">
+    <label :for="id" class="block text-gray-600 text-sm mb-1">{{
+      label
+    }}</label>
+    <input
+      :type="type"
+      class="border p-2 rounded block w-full"
+      :id="id"
+      v-model="val"
+      :required="required"
+      :disabled="disabled"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "FormInput",
+  props: {
+    value: {
+      default: "",
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  mounted() {
+    this.id = Math.random().toString(36).substring(7);
+  },
+  data() {
+    return {
+      val: this.value,
+      id: null,
+    };
+  },
+  watch: {
+    value(v) {
+      this.val = v;
+    },
+    val(v) {
+      this.$emit(v);
+    },
+  },
+};
+</script>

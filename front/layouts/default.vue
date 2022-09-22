e352b815429aba1ff7477ced1dd6cc1de64f4e5f
Dave Njoroge <davenjoroge10@gmail.com>
2022-09-22 12:56:09 +0300

    initial setup, layout

---
+++ front/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-full">
+    <header class="bg-gray-800 text-white p-4">
+        <div class="container mx-auto">
+            <div class="flex">
+              <nuxt-link to="/" class="font-medium">Socialite</nuxt-link>
+              <div class="ml-auto space-x-4 flex items-center">
+                <nuxt-link to="/login" class="">Login</nuxt-link>
+                <span class="border-r h-5"></span>
+                <nuxt-link to="/Register" class="">Register</nuxt-link>
+              </div>
+            </div>
+        </div>
+    </header>
+    <main class="py-4">
+        <div class="container mx-auto">
+            <nuxt />
+        </div>
+    </main>
+  </div>
+</template>
+<script>
+name: "Layout";
+</script>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1 class="text-4xl mb-3">Home</h1>
+    <p class="text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati facilis voluptate ducimus ipsum eum dicta quaerat libero non vitae laborum. Illo deserunt magni vero nemo labore facere maiores laboriosam quisquam!</p>
+  </div>
 </template>
 
 <script>
-export default {}
+import Tutorial from '~/components/Tutorial.vue';
+export default { components: { Tutorial } }
 </script>

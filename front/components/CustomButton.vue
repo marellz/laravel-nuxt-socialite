a0745728b481fd66f869fddc0f24af2d8453d2b3
Dave Njoroge <davenjoroge10@gmail.com>
2022-09-23 11:04:03 +0300

    fix

--- front/components/CustomButton.vue
+++ front/components/CustomButton.vue
@@ -1,5 +1,8 @@
 <template>
-    <button class="py-2 px-5 text-white inline-block font-medium rounded bg-blue-500">
-        <slot/>
-    </button>
+  <button
+    @click="$emit('click')"
+    class="py-2 px-5 text-white inline-block font-medium rounded bg-blue-500"
+  >
+    <slot />
+  </button>
 </template>
